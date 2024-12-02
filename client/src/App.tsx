6ebf004d4d0207325f4be957976e5f3ba0eb84b1
Evan Zhang <zhangevan82@gmail.com>
2024-12-01 19:14:21 -0500

    Add Medical Records page with search and filter functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import {Patients} from './pages/Patients/Patients';
 import Appointments from './pages/Appointments/Appointments';
+import MedicalRecords from './pages/MedicalRecords/MedicalRecords';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/patients' element={<Patients />}/>
           <Route path='/appointments' element={<Appointments />}/>
+          <Route path='/records' element={<MedicalRecords />} />
         </Routes>
       
       </BrowserRouter>
